d01e7626f2376210b64fbc6ebaa965071635cc96
Colin M <colinm22283@isd273.org>
2021-09-22 14:01:04 -0400

    1.0

--- scripts/source/main.cpp
+++ scripts/source/main.cpp
@@ -9,6 +9,9 @@
 #include "input.h"
 #include "physics.h"
 #include "consoleParser.h"
+#include "scriptGlobal.h"
+
+bool erase = false;
 
 void Script::exit()
 {
@@ -22,32 +25,47 @@ float blockPos = 0;
 
 void Script::start()
 {
-    Physics::init(100, 100);
+    Physics::init(200, 150);
 
-    Global::fpsLimit = 20;
     Global::fpsOutput = true;
 }
 void Script::update()
 {
     Render::clearScreen({ 0, 0, 0, 255 });
     Physics::update();
-    Physics::render(0, 0, 5);
+    Physics::render(0, 0, ScriptGlobal::drawScale);
+
+    Render::drawRect(
+        Input::mouseX - (10 * ScriptGlobal::drawScale),
+        Input::mouseY - (10 * ScriptGlobal::drawScale),
+        20 * ScriptGlobal::drawScale, 20 * ScriptGlobal::drawScale, { 255, 255, 255, 255 }
+    );
 }
 
 void Script::mouseDown(int button)
 {
     for (
-        int i = std::max(0, Input::mouseX / 5 - 10);
-        i < std::min(Physics::boardWidth - 1, Input::mouseX / 5 + 10);
+        int i = std::max(0, Input::mouseX / ScriptGlobal::drawScale - 10);
+        i < std::min(Physics::boardWidth - 1, Input::mouseX / ScriptGlobal::drawScale + 10);
         i++
     ) {
         for (
-            int j = std::max(0, Input::mouseY / 5 - 10);
-            j < std::min(Physics::boardHeight - 1, Input::mouseY / 5 + 10);
+            int j = std::max(0, Input::mouseY / ScriptGlobal::drawScale - 10);
+            j < std::min(Physics::boardHeight - 1, Input::mouseY / ScriptGlobal::drawScale + 10);
             j++
         ) {
-            Physics::board[i][j] = 1;
+            if (button == SDL_BUTTON_LEFT)
+            {
+                if (erase) Physics::board[i][j] = 0;
+                else Physics::board[i][j] = 1;
+            }
         }
     }
 }
-void Script::mouseClick(int button) { }
+void Script::mouseClick(int button)
+{
+    if (button == SDL_BUTTON_RIGHT)
+    {
+        erase = !erase;
+    }
+}
